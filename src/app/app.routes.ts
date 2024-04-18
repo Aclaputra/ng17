ccf1d92b8e57bb78fc42f521ee14251502aee36c
Muhammad Acla <aclalead@gmail.com>
2024-04-18 15:15:01 +0700

    feat: add routes and color

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        title: 'Home Page',
+        component: HomeComponent,
+    },
+    {
+        path: '**',
+        title: '404 Not Found',
+        component: NotFoundComponent
+    },
+];

dabfc5d535e2825f91fca79d328d7213f14d791f
Muhammad Acla <aclalead@gmail.com>
2024-04-18 15:45:46 +0700

    feat: add about and project page router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,24 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 export const routes: Routes = [
     {
         path: '',
         title: 'Home Page',
         component: HomeComponent,
+    },
+    {
+        path: 'about',
+        title: 'About Page',
+        component: AboutComponent,
+    },
+    {
+        path: 'projects',
+        title: 'Projects Page',
+        component: ProjectsComponent,
     },
     {
         path: '**',
